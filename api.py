1e5538ad6254b518383bff5cdb6bf925eba626aa
Zhredder <49133393+Zhredder@users.noreply.github.com>
2024-04-15 19:12:13 +1200

    api file added

---
+++ api.py
@@ -0,0 +1,19 @@
+import requests
+
+#! This file fetches cryptocurrency price data from an API #!
+
+def get_crypto_data():
+    url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+
+
+def main():
+
+    get_crypto_data()
+
+if __name__ == "__main__":
+    main()
